907f01264703777360265d9f261b1d825ea7256f
Bjørn Fjukstad <bjorn@cs.uit.no>
2017-01-11 16:54:49 -0500

    now also running the go present tool

--- tools/repo-man.sh
+++ tools/repo-man.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 URL=$1
+BASEDIR=$2
 if [$URL == ""]
 then 
     echo "Please provide a repo url (http://... ending in .zip"
     exit 1
 fi 
 
+if [$BASEDIR == ""]
+then 
+    echo "Please provide a base directory for the present tool"
+    exit 1
+fi 
+
 TALKDIR=/tmp/talks
 mkdir $TALKDIR
+cd $TALKDIR 
 
+# Start the go present tool. Should probably also check that the present tool
+# is installed, but whatevs.
+present -base $BASEDIR &
+
+# Continuously download talks from the given repo url 
 while true
 do 
     wget -O /tmp/repo.zip $URL
-    unzip -u /tmp/repo -d $TALKDIR
+    unzip -u -o /tmp/repo -d $TALKDIR
     sleep 300
 done 
